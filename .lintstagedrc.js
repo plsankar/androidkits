5862402410af090d544dff63d60b70f66b986100
Lakshmi Sankar <plsankar1996@gmail.com>
2023-09-04 23:46:39 +0530

    chore: updated lint-staged

--- .lintstagedrc.js
+++ .lintstagedrc.js
@@ -1,11 +1,11 @@
 const path = require("path");
 
 const buildEslintCommand = (filenames) => {
-	return `next lint --fix --max-warnings 0 --file  ${filenames
-		.map((f) => path.relative(process.cwd(), f))
-		.join(" --file ")}`;
+  return `next lint --fix --max-warnings 0 --file  ${filenames
+    .map((f) => path.relative(process.cwd(), f))
+    .join(" --file ")}`;
 };
 
 module.exports = {
-	"*.{js,jsx,ts,tsx}": [buildEslintCommand],
+  "*.{js,jsx,ts,tsx},!.lintstagedrc.js": [buildEslintCommand],
 };
