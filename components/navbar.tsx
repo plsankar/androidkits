fdcb34c941ea67c513cbcb3d9ba8e561a92fe510
Lakshmi Sankar <plsankar1996@gmail.com>
2023-09-04 23:09:02 +0530

    feat: added navbar and logo

---
+++ components/navbar.tsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import Logo from './logo';
+import { ModeToggle } from './ui/mode-toggle';
+
+const Navbar = () => {
+  return (
+    <div className="py-4 border-b w-full">
+      <div className="container flex flex-row gap-5 justify-between items-center">
+        <div>
+          <Logo className="h-8 w-auto" />
+        </div>
+        <div>
+          <ModeToggle />
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Navbar;
