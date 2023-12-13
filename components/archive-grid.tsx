3fbba2a94e11a9c2bfa87b9b4f5ff0cddbcb70b1
Lakshmi Sankar <plsankar1996@gmail.com>
2023-12-13 22:22:08 +0530

    refactor: updated types

--- components/archive-grid.tsx
+++ components/archive-grid.tsx
@@ -1,6 +1,6 @@
 import React, { FC } from "react";
 import ArchiveItem from "./archive-item";
-import { ProjectWithUser } from "@/additional";
+import { ProjectWithUser } from "../additional";
 
 const ArchiveGrid: FC<{ projects: ProjectWithUser[] }> = ({ projects }) => {
     return (
