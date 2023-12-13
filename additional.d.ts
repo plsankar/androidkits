8603cf6581a018e49abc1de91b9ac312a6267033
Lakshmi Sankar <plsankar1996@gmail.com>
2023-12-13 22:36:23 +0530

    refactor: moved type modules

--- additional.d.ts
+++ additional.d.ts
@@ -1,9 +1,5 @@
 import type { Prisma } from "@prisma/client";
 
-type ProjectWithUser = Prisma.ProjectGetPayload<{
+export type ProjectWithUser = Prisma.ProjectGetPayload<{
     include: { user: true };
 }>;
-
-declare module "rehype-add-classes";
-declare module "@microflash/rehype-starry-night";
-declare module "rehype-urls";
