05de818097ab326fa4520e4fc2e5c208bb78377f
Lakshmi Sankar <plsankar1996@gmail.com>
2023-12-13 21:15:52 +0530

    refactor: added link to navbar logo

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -1,13 +1,17 @@
 import React from "react";
 import Logo from "./logo";
 import { ModeToggle } from "./ui/mode-toggle";
+import Link from "next/link";
 
 const Navbar = () => {
     return (
         <div className="py-4 border-b w-full">
             <div className="container flex flex-row gap-5 justify-between items-center">
                 <div>
-                    <Logo className="h-8 w-auto" />
+                    <Link href="/" className="text-left inline-block">
+                        <Logo className="h-8 w-auto" />
+                        <span className="sr-only">AndroidKits</span>
+                    </Link>
                 </div>
                 <div>
                     <ModeToggle />
