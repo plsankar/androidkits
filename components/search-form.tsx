e5e04e496625362cc3367db430db1432b090719d
Lakshmi Sankar <plsankar1996@gmail.com>
2023-12-24 23:25:19 +0530

    refactor: removed usequeryparams from searchform

--- components/search-form.tsx
+++ components/search-form.tsx
@@ -1,20 +1,10 @@
-"use client";
-
 import React from "react";
 import { Input } from "@/components/ui/input";
-import { useSearchParams } from "next/navigation";
 
 const SearchForm = () => {
-    const params = useSearchParams();
     return (
         <form action="/search" method="get">
-            <Input
-                title="Search..."
-                className="w-[200px]"
-                name="query"
-                placeholder="Search..."
-                defaultValue={params?.get("query") || ""}
-            />
+            <Input title="Search..." className="w-[200px]" name="query" placeholder="Search..." defaultValue={""} />
         </form>
     );
 };
